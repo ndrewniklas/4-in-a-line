a18bbb8950dfda1cc3e35f55db8f698e7ce8675b
amnipp <amnipp@cpp.edu>
2017-06-04 23:28:44 -0700

    I forgot to readd the AI's place piece function, opps. Fixed.

--- 4-in-a-line.cpp
+++ 4-in-a-line.cpp
@@ -79,11 +79,10 @@ void callback() {
 }
 bool botTakeTurn(char player, UserInterface ui, Board* game) {
 	//Do bot stuff, currently just another player
-	Timer t;
-	t.Start(chrono::seconds(botThinkTime),callback);
 	string pPos = ai->search(game);
 	//string pPos = ui.enterPosition();
 	cout << pPos << endl;
+	game->setPiece(pPos[0], atoi(&pPos[1]), player);
 	cout << *(game);
 	cout << "Score:" << game->calculateScore() << endl;
 	if (game->checkWinCondition(pPos[0], atoi(&pPos[1]))) {
